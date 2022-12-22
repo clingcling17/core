51d1c0af329f47bbbfe8fcda40bb6150ddc05a2f
seungyeon_heo <seungyeon_heo@tmax.co.kr>
2022-12-22 18:57:39 +0900

    싱글톤 방식의 주의점

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,7 +2,7 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-//    private int price; //상태를 유지하는 필드 10000 -> 20000
+//    private int price; //상태를 유지하는 필드 10000 -> 20000 (공유필드)
 
     public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
