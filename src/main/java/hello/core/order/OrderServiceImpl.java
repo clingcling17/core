a6fb3d7a0188dcbf873125757e107853ea551c83
SeungYeon Heo <viophet@gmail.com>
2022-12-22 21:12:00 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,9 +7,6 @@ import hello.core.member.MemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드를 고쳐야 한다.
-                                                                                  // (상위 모듈이 하위 모듈의 추상이 아닌 구체에 의존) DIP 위반. -> OCP 위반.
     private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
@@ -23,5 +20,10 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice); //단일 체계 원칙(SRP)
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
